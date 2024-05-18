7c1e311e92f1b45e3d5edbcd088b657e8f9890a3
vdavdov <sevadavydov.06z@mail.ru>
2024-05-18 16:40:23 +0400

    Solve Palindrome Number

--- src/main/java/leetcode/PalindromeNumber9/Solution.java
+++ src/main/java/leetcode/PalindromeNumber9/Solution.java
@@ -1,4 +1,22 @@
 package leetcode.PalindromeNumber9;
 
 public class Solution {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(123));
+        System.out.println(isPalindrome(-23));
+        System.out.println(isPalindrome(323));
+    }
+
+    public static boolean isPalindrome(int x) {
+        String str = String.valueOf(x);
+        int len = str.length();
+
+        for (int i = 0; i < len/2; i++) {
+            if (str.charAt(i) != str.charAt(len-1-i)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
