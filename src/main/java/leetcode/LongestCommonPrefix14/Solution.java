b6087c1299508debc90b386740c985943edda514
vdavdov <sevadavydov.06z@mail.ru>
2024-05-23 20:28:51 +0400

    Solve LongestCommonPrefix14

---
+++ src/main/java/leetcode/LongestCommonPrefix14/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.LongestCommonPrefix14;
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println("prefix is: " + longestCommonPrefix(new String[]{""}));
+        System.out.println("prefix is: " + longestCommonPrefix(new String[]{"flower", "flow", "flight"}));
+        System.out.println("prefix is: " + longestCommonPrefix(new String[]{"dog", "racecar", "car"}));
+    }
+
+    public static String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length - 1];
+        int index = 0;
+
+        while (index < first.length() && index < last.length()) {
+            if (first.charAt(index) == last.charAt(index)) {
+                index++;
+            } else {
+                return first.substring(0, index);
+            }
+        }
+        return first.substring(0, index);
+    }
+}
--- src/main/java/leetcode/LongestSubstringWithoutRepeating3/Solution.java
+++ src/main/java/leetcode/LongestSubstringWithoutRepeating3/Solution.java
@@ -1,23 +1,23 @@
-//package leetcode.LongestSubstringWithoutRepeating3;
-//
-//import java.util.HashSet;
-//import java.util.Set;
-//
-//public class Solution {
-//    public static void main(String[] args) {
-//        System.out.println(lengthOfLongestSubstring("pwwekw"));
-//    }
-//
-//    public static int lengthOfLongestSubstring(String s) {
-//        int stringLength = s.length();
-//        int maxLength = 0;
-//        Set<Character> set = new HashSet<>();
-//
-//        for (int right = 0; right < stringLength; right++) {
-//            set.add(s.charAt(right));
-//
-//        }
-//
-//        return
-//    }
-//}
+package leetcode.LongestSubstringWithoutRepeating3;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLongestSubstring("pwwekw"));
+    }
+
+    public static int lengthOfLongestSubstring(String s) {
+        int stringLength = s.length();
+        int maxLength = 0;
+        Set<Character> set = new HashSet<>();
+
+        for (int right = 0; right < stringLength; right++) {
+            set.add(s.charAt(right));
+
+        }
+
+        return 0;
+    }
+}
